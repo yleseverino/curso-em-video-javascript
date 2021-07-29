f7bf89040dd14940f6248561bbcf2b2d23716957
yleseverino <ylewap0@gmail.com>
2021-07-29 14:10:13 -0300

    adicionando os arquivos

---
+++ aula11/ex009.js
@@ -0,0 +1,2 @@
+var vel = 65
+console.log(`A velocidade do seu carro é ${vel} km/h`)
---
+++ aula12/ex010.js
@@ -0,0 +1,7 @@
+var idade = 4
+if (idade < 18 && idade > 9) {
+    console.log("teste")
+} else {
+    console.log("sim")
+}
+    
